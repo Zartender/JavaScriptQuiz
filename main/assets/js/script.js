277d58f1e078daba86de73891642578c08a10995
Ian Greenblott <imgreenblott@gmail.com>
2021-10-11 23:32:16 -0400

    edits to indexhtml y scriptjs.

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -1,157 +1,158 @@
 
-(function(){
+(function () {
     // Functions
-    function bobBuildQuiz(){
-      // variable to store the HTML output
-      const output = [];
-  
-      // for each question...
-      myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
-          const answers = [];
-  
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
+    function bobBuildQuiz() {
+        // variable to store the HTML output
+        console.log(bobBuildQuiz);
+        const output = [];
+
+        // for each question...
+        myQuestions.forEach(
+            
+            (currentQuestion, questionNumber) => {
+
+                // variable to store the list of possible answers
+                const answers = [];
+
+                // and for each available answer...
+                for (letter in currentQuestion.answers) {
+
+                    // ...add an HTML radio button
+                    answers.push(
+                        `<label>
                 <input type="radio" name="question${questionNumber}" value="${letter}">
                 ${letter} :
                 ${currentQuestion.answers[letter]}
               </label>`
-            );
-          }
-  
-          // add this question and its answers to the output
-          output.push(
-            `<div class="slide">
+                    );
+                }
+
+                // add this question and its answers to the output
+                output.push(
+                    `<div class="slide">
               <div class="question"> ${currentQuestion.question} </div>
               <div class="answers"> ${answers.join("")} </div>
             </div>`
-          );
-        }
-      );
-  
-      // finally combine our output list into one string of HTML and put it on the page
-      qqqContainer.innerHTML = output.join('');
+                );
+            }
+        );
+
+        // finally combine our output list into one string of HTML and put it on the page
+        qqqContainer.innerHTML = output.join('');
     }
-  
-    function showResults(){
-  
-      // gather answer containers from our quiz
-      const answerContainers = qqqContainer.querySelectorAll('.answers');
-  
-      // keep track of user's answers
-      let numCorrect = 0;
-  
-      // for each question...
-      myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        // if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer){
-          // add to the number of correct answers
-          numCorrect++;
-  
-          // color the answers green
-          answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else{
-          // color the answers red
-          answerContainers[questionNumber].style.color = 'red';
-        }
-      });
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+
+    function showResults() {
+
+        // gather answer containers from our quiz
+        const answerContainers = qqqContainer.querySelectorAll('.answers');
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else {
+                // color the answers red
+                answerContainers[questionNumber].style.color = 'red';
+            }
+        });
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
     }
-  
+
     function showSlide(n) {
-      slides[currentSlide].classList.remove('active-slide');
-      slides[n].classList.add('active-slide');
-      currentSlide = n;
-      if(currentSlide === 0){
-        previousButton.style.display = 'none';
-      }
-      else{
-        previousButton.style.display = 'inline-block';
-      }
-      if(currentSlide === slides.length-1){
-        nextButton.style.display = 'none';
-        submitButton.style.display = 'inline-block';
-      }
-      else{
-        nextButton.style.display = 'inline-block';
-        submitButton.style.display = 'none';
-      }
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        if (currentSlide === 0) {
+            previousButton.style.display = 'none';
+        }
+        else {
+            previousButton.style.display = 'inline-block';
+        }
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        }
+        else {
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
     }
-  
+
     function showNextSlide() {
-      showSlide(currentSlide + 1);
+        showSlide(currentSlide + 1);
     }
-  
+
     function showPreviousSlide() {
-      showSlide(currentSlide - 1);
+        showSlide(currentSlide - 1);
     }
-  
+
     // Variables
     const qqqContainer = document.getElementById('quiz');
     const resultsContainer = document.getElementById('results');
     const submitButton = document.getElementById('submit');
     const myQuestions = [
-      {
-        question: "Who invented JavaScript?",
-        answers: {
-          a: "Mark Twain",
-          b: "Joseph Stihl",
-          c: "Brendan Eich"
+        {
+            question: "Who invented JavaScript?",
+            answers: {
+                a: "Mark Twain",
+                b: "Joseph Stihl",
+                c: "Brendan Eich"
+            },
+            correctAnswer: "c"
         },
-        correctAnswer: "c"
-      },
-      {
-        question: "What is JavaScript best utilized for?",
-        answers: {
-          a: "Making pages dynamic",
-          b: "Building forts",
-          c: "Creating paper airplanes"
+        {
+            question: "What is JavaScript best utilized for?",
+            answers: {
+                a: "Making pages dynamic",
+                b: "Building forts",
+                c: "Creating paper airplanes"
+            },
+            correctAnswer: "a"
         },
-        correctAnswer: "a"
-      },
-      {
-        question: "Which tool does Javascript pair like 'lamb and tuna fish' with?",
-        answers: {
-          a: "Angular",
-          b: "jQuery",
-          c: "RequireJS",
-          d: "ESLint"
-        },
-        correctAnswer: "b"
-      }
+        {
+            question: "Which tool does Javascript pair like 'lamb and tuna fish' with?",
+            answers: {
+                a: "Angular",
+                b: "jQuery",
+                c: "RequireJS",
+                d: "ESLint"
+            },
+            correctAnswer: "b"
+        }
     ];
-  
+
     // Kick things off
     bobBuildQuiz();
-  
+
     // Pagination
     const previousButton = document.getElementById("prev");
     const nextButton = document.getElementById("next");
     const slides = document.querySelectorAll(".slide");
     let currentSlide = 0;
-  
+
     // Show the first slide
     showSlide(currentSlide);
-  
+
     // Event listeners
     submitButton.addEventListener('click', showResults);
     previousButton.addEventListener("click", showPreviousSlide);
     nextButton.addEventListener("click", showNextSlide);
-  })();
-  
+})();
