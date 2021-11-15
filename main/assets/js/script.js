a8ece809438eb5ed609607ab1c06ab75d887560e
Ian Greenblott <imgreenblott@gmail.com>
2021-11-15 00:47:01 -0500

    Finally, the clock is running. GL.

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -1,11 +1,22 @@
 // queryselector inputs time into p with class timer
-let timer = document.querySelector("p.timer");
+
 let questionRemaining = 0;
 let myQuestions = document.querySelector("#myQuestions");
 // objects related to time
 let timeFinished = document.querySelector("#timeFinished");
 let contender = document.querySelector("#contender");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector("p.timer-count");
+;
 
+var chosenWord = "";
+var numBlanks = 0;
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
 // start button to begin quiz
 const startButton = document.querySelector("#begin");
 
@@ -15,11 +26,13 @@ const answerThree = document.querySelector("#threeAnswer");
 
 function startQuiz() {
     console.log("Begin Quiz");
+    timerCount = 85;
+    startTimer();
     
 }
 
 // on start
-startButton.addEventListener("click", startQuiz, setTime);
+startButton.addEventListener("click", startQuiz);
 
 
 // array of my questions, possible answers, and correct answer.
@@ -56,198 +69,24 @@ const questionaires = [
 ];
 
 // timer function trouble
-function setTime() {
-    let timeFrameInterval = setInterval (function() {
-        remainingSeconds--;
-        timer.textContent = `Timer:${remainingSeconds}`;
-        if(remainingSeconds === 0 || questionRemaining === questionaires.length) {
-            clearInterval(timeFrameInterval);
-            myQuestions.display = "none";
-            timeFinished.display = "block";
-            contender.textContent = remainingSeconds;
-            
-
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          winGame();
         }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        loseGame();
+      }
     }, 1000);
-    
-}
-
-// // begin quiz
-// function startQuiz() {
-//     // set question
-//     console.log("begin");
-//     // begin clock
-// }
-
-
-
-
-
-// (() => {
-    
-//     function CreateQuiz() {
-//         var output = [];
-//         myQuestions.forEach(
-
-//             (currentQuestion, questionNumber) => {
-
-                
-//                 var answers = [];
-
-//                 for (letter in currentQuestion.answers) {
-
-//                     answers.push(
-//                         `<label>
-//                 <input type="radio" name="question${questionNumber}" value="${letter}">
-//                 ${letter} :
-//                 ${currentQuestion.answers[letter]}
-//               </label>`
-//                     );
-//                 }
-
-            
-//                 output.push(
-//                     `<div class="slide">
-//               <div class="question"> ${currentQuestion.question} </div>
-//               <div class="answers"> ${answers.join("")} </div>
-//             </div>`
-//                 );
-//             }
-//         );
-
-        
-//         qContainer.innerHTML = output.join('');
-//     }
-
-//     function showResults() {
-
-//         // gather answer containers from our quiz
-//         const answerContainers = qContainer.querySelectorAll('.answers');
-
-//         // keep track of user's answers
-//         let numCorrect = 0;
-
-//         // for each question...
-//         myQuestions.forEach((currentQuestion, questionNumber) => {
-
-//             // find selected answer
-//             const answerContainer = answerContainers[questionNumber];
-//             const selector = `input[name=question${questionNumber}]:checked`;
-//             const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-//             // if answer is correct
-//             if (userAnswer === currentQuestion.correctAnswer) {
-//                 // add to the number of correct answers
-//                 numCorrect++;
-
-//                 // color the answers green
-//                 answerContainers[questionNumber].style.color = 'lightgreen';
-//             }
-
-//             // if answer is wrong or blank
-//             else {
-//                 // color the answers red
-//                 answerContainers[questionNumber].style.color = 'red';
-//             }
-//         });
-
-//         // show number of correct answers out of total
-//         resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-//     }
-
-//     function showSlide(n) {
-//         slides[currentSlide].classList.remove('active-slide');
-//         slides[n].classList.add('active-slide');
-//         currentSlide = n;
-//         if (currentSlide === 0) {
-//             previousButton.style.display = 'none';
-//         }
-//         else {
-//             previousButton.style.display = 'inline-block';
-//         }
-//         if (currentSlide === slides.length - 1) {
-//             nextButton.style.display = 'none';
-//             submitButton.style.display = 'inline-block';
-//         }
-//         else {
-//             nextButton.style.display = 'inline-block';
-//             submitButton.style.display = 'none';
-//         }
-//     }
-
-//     function showNextSlide() {
-//         showSlide(currentSlide + 1);
-//     }
-
-//     function showPreviousSlide() {
-//         showSlide(currentSlide - 1);
-//     }
-
-//     function startQuiz() {
-//         //Hide the start screen 
-//         var divStartEL = document.getElementById("startScreen"); 
-//         divStartEL.setAttribute("class", "hide");
-//         //un-hide the question screen 
-
-//         qContainer.removeAttribute('class');
-//         nextButton.style.display = 'inline-block';
-
-//     }
-    
-//     var qContainer = document.getElementById('quiz');
-//     var resultsContainer = document.getElementById('results');
-//     var submitButton = document.getElementById('submit');
-//     var startButton = document.getElementById('begin');
-//     var myQuestions = [
-        
-        
-//         {
-//             question: "Who invented JavaScript?",
-//             answers: {
-//                 a: "Mark Twain",
-//                 b: "Joseph Stihl",
-//                 c: "Brendan Eich"
-//             },
-//             correctAnswer: "c"
-//         },
-//         {
-//             question: "What is JavaScript best utilized for?",
-//             answers: {
-//                 a: "Making pages dynamic",
-//                 b: "Building forts",
-//                 c: "Creating paper airplanes"
-//             },
-//             correctAnswer: "a"
-//         },
-//         {
-//             question: "Which tool does Javascript pair like 'lamb and tuna fish' with?",
-//             answers: {
-//                 a: "Angular",
-//                 b: "jQuery",
-//                 c: "RequireJS",
-//                 d: "ESLint"
-//             },
-//             correctAnswer: "b"
-//         }
-//     ];
-
-    
-//     CreateQuiz();
-
-    
-//     var previousButton = document.getElementById("prev");
-//     var nextButton = document.getElementById("next");
-//     var slides = document.querySelectorAll(".slide");
-//     let currentSlide = 0;
-
-    
-//     showSlide(currentSlide);
-//     nextButton.style.display = 'none';
-
-
-//     submitButton.addEventListener('click', showResults);
-//     previousButton.addEventListener("click", showPreviousSlide);
-//     nextButton.addEventListener("click", showNextSlide);
-//     startButton.addEventListener('click', startQuiz); 
-// })();
-
+  }
