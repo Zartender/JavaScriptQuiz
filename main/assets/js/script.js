db652b23f97d67b30e3de71173d48c301f401ca2
Ian Greenblott <imgreenblott@gmail.com>
2021-12-08 23:30:01 -0500

    Edited Javascript functionality. Added else if statments for displaying questions. Added Timer

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -30,10 +30,9 @@ let Q5 = {
   Correct: 3,
 }
 
-let Q6 = {
-  Q: "Which logical operator means 'or'?",
-  As: ["@@", "&&", "##", "||"],
-  Correct: 4,
+let Q6 = { "Which statement creates a loops that continues to run as long as a specified condition is true?",
+  As: ["while", "if", "var", "for"],
+  Correct: 1,
 }
 
 let Q7 = {
@@ -55,15 +54,20 @@ let Q9 = {
 }
 
 let Q10 = {
-  Q: "Which statement creates a loops that continues to run as long as a specified condition is true?",
-  As: ["while", "if", "var", "for"],
-  Correct: 1,
+  Q: "Which logical operator means 'or'?",
+  As: ["@@", "&&", "##", "||"],
+  Correct: 4,
 }
 
-var options = $('.options');
+
+
+
+
+
+let options = $('.options');
 options.on('click', '.option', chooseSelection);
 
-//var button = $('.input');
+//letbutton = $('.input');
 //button.on('click', '.input', enterInitials);
 
 //Displays Question and Options from Object entered
@@ -84,3 +88,123 @@ function displayQuestion(question) {
       third.textContent = question.As[2];
       fourth.textContent = question.As[3];
   }
+
+  const Key = [2,4,3,4,3,4,1,2,2,1];
+let qNumber = 0;
+
+function chooseSelection(event){
+  let chosen = event.target.value;
+  // Keeps track of cor/incorrect answers.
+  if(chosen == Key[qNumber - 1]){ 
+      score[0]++;
+      nextQuestion();
+  } else {
+      score[1]++;
+      nextQuestion();
+      timeLeft = timeLeft-5;
+  }
+
+}
+//injects in Question elements to correct attributes while the clock runs
+function selectQ(qNumber){ 
+  if (qNumber == 1){
+      displayQ(Q1);
+  } else if (qNumber == 2){
+      displayQ(Q2);
+  } else if (qNumber == 3){
+      displayQ(Q3);
+  } else if (qNumber == 4){
+      displayQ(Q4);
+  } else if (qNumber == 5){
+      displayQ(Q5);
+  } else if (qNumber == 6){
+      displayQ(Q6);
+  } else if (qNumber == 7){
+      displayQ(Q7);
+  } else if (qNumber == 8){
+      displayQ(Q8);
+  } else if (qNumber == 9){
+      displayQ(Q9);
+  } else if (qNumber == 10){
+      displayQ(Q10);
+  }  
+  else {
+      clearOptions();
+      showScore();
+  }
+}
+
+function clearOptions(){
+  let Q = $('.question');
+  Q.empty();
+  let O = $('.option')
+  O.parent().empty();
+  spawnTextbox();
+}
+//Display score at the end
+function showScore(){ 
+  let show = $('#scoreboard')
+  show.text("You got " + score[0] + " correct and " + score[1] + " wrong.");
+}
+
+function nextQuestion (){ 
+  qNumber++; 
+  selectQuestion(qNumber);
+}
+
+//Save score and initials to local storage
+function spawnTextbox(){
+  let div = $('.inits');
+  let input = $('<input>');
+  let message = $('<h3>');
+  let button = $('<button>');
+  button.addClass('input');
+  button.text('Submit');
+  
+  message.text('Enter your initials!')
+  div.append(message);
+  div.append(input);
+  div.append(button);
+  button.on('click', '.input', enterInitials);
+}
+
+
+function enterInitials(){
+  let div = $('.inits');
+  let input = div.children().eq(1);
+  initials = input.value;
+  console.log(initials);
+
+  localStorage.setItem("#score", score[0].toString());
+  localStorage.setItem("#initials", initials);
+}
+
+
+// Timer Code
+
+let timerEl = document.getElementById('countdown');
+let timeLeft = 0;
+let timeInterval = 0;
+
+function countdown() {
+  timeLeft = 60;
+
+  //
+  timeInterval = setInterval(function () {
+      timeLeft--;
+      timerEl.textContent = timeLeft + " seconds left";
+      if (qNumber == 11){
+          clearInterval(timeInterval);
+      } 
+      if (timeLeft == 0) {
+          clearInterval(timeInterval);
+          if (qNumber !== 11) {
+          clearOptions();
+          showScore();
+          }
+      }
+  }, 1000);
+}
+
+nextQuestion();
+countdown();
